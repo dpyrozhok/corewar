f184ec712e95605fb89f72a4f7cc359acbfa7c50
Denis PYROZHOK <dpyrozho@e1r8p14.unit.ua>
2018-08-10 18:08:06 +0300

    queery

--- asm.c
+++ asm.c
@@ -11,6 +11,8 @@
 /* ************************************************************************** */
 #include "asm.h"
 
+int g_fd;
+
 int		ft_check_format(char *name)
 {
     char *format;
@@ -26,13 +28,63 @@ char		*ft_get_name(char *name)
 {
 	char	*get;
 	int		i;
+	char	*tmp;
 
 	i = 0;
 	while (name[i] != '.')
 		i++;
 	get = malloc(sizeof(char) * i);
 	ft_strncpy(get, name, i);
+	tmp = get;
+	get = ft_strjoin(get, ".cor");
+	free(tmp);
 	return (get);
+}
+
+static int	is_little(void)
+{
+	char		a;
+
+	a = 1;
+	if (a >> 2 == 0)
+		return (1);
+	return (0);
+}
+
+static int	swap_two_octets(int nb)
+{
+	int		tmp;
+
+	tmp = nb & 0xff;
+	nb = (nb & 0xff00) / 0xff;
+	nb = nb + (tmp * 0x100);
+	return (nb);
+}
+
+int		little_to_big_endian(int nb)
+{
+	int tmp;
+
+	if (is_little() == 1)
+	{
+		tmp = nb & 0xffff;
+		nb = (nb & 0xffff0000) / 0xffff;
+		tmp = swap_two_octets(tmp) * 0x10000;
+		nb = swap_two_octets(nb) + tmp;
+	}
+	return (nb);
+}
+
+void		ft_read_file(char *name)
+{
+	int su = little_to_big_endian(COREWAR_EXEC_MAGIC);
+	if ((g_fd = open(name, O_WRONLY | O_CREAT | O_TRUNC,
+					   S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH)) == -1)
+	{
+		ft_printf("error: open\n");
+		exit(1);
+	}
+	write(g_fd, &su, sizeof(su));
 }
 
 int		main(int ac, char **av)
@@ -41,12 +93,12 @@ int		main(int ac, char **av)
 
 	if (ac < 2 || ac > 3)
 		return (ft_printf("Invalid number of arguements")); // inform invalid number of arguement
-	if (!(ft_check_format(av[1])))
+	if (!ft_check_format(av[1]))
 		return(ft_printf("Can't read source file\n"));
 	else
 		name = ft_get_name(av[1]);
-	ft_printf("Writing output program to ");
-	ft_printf("%s.cor", name);
+	ft_read_file(name);
+	ft_printf("Writing output program to %s", name);
 	free(name);
 	return (0);
 }
