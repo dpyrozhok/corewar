1dbcc18b3653b4a2b00e891c912f757208c8bd7c
Denis PYROZHOK <dpyrozho@e1r8p14.unit.ua>
2018-08-10 10:40:20 +0300

    checkcheck

---
+++ asm.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   asm.h                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dpyrozho <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/08/09 14:49:21 by dpyrozho          #+#    #+#             */
+/*   Updated: 2018/08/09 14:49:23 by dpyrozho         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ASM_H
+# define ASM_H
+# define OP(index)	g_op_tab[index]
+
+# include "libft/libft.h"
+# include "ft_printf/libftprintf.h"
+# include "op.h"
+# include <fcntl.h>
+# include <string.h>
+# include <stdio.h>
+# include <fcntl.h>
+
+#endif
