f7f43ff3d09c870244d93eb4657743c7c15e3e5f
Anastasiia MALKEVYCH <amalkevy@e1r7p11.unit.ua>
2018-08-16 12:13:49 +0300

    name and comment checking

---
+++ asm.c
@@ -0,0 +1,355 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   asm.c                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dpyrozho <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/08/09 13:13:07 by dpyrozho          #+#    #+#             */
+/*   Updated: 2018/08/09 13:13:09 by dpyrozho         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include "asm.h"
+
+int g_fd;
+int g_fd1;
+
+int		ft_check_format(char *name)
+{
+    char *format;
+
+    format = ft_strchr(name, '.');
+    if (ft_strcmp(format, ".s") == 0)
+        return (1);
+    else
+        return (0);
+}
+
+char		*ft_get_name(char *name)
+{
+	char	*get;
+	int		i;
+	char	*tmp;
+
+	i = 0;
+	while (name[i] != '.')
+		i++;
+	get = malloc(sizeof(char) * i);
+	ft_strncpy(get, name, i);
+	tmp = get;
+	get = ft_strjoin(get, ".cor");
+	free(tmp);
+	return (get);
+}
+
+static int	is_little(void)
+{
+	char		a;
+
+	a = 1;
+	if (a >> 2 == 0)
+		return (1);
+	return (0);
+}
+
+int	ft_change(int ch)
+{
+	int		new;
+
+	new = ch & 0xff;
+	ch = (ch & 0xff00) / 0xff;
+	ch = ch + (new * 0x100);
+	return (ch);
+}
+
+int		convert_end(int new)
+{
+	int ch;
+
+	if (is_little() == 1)
+	{
+		ch = new & 0xffff;
+		new = (new & 0xffff0000) / 0xffff;
+		ch = ft_change(ch) * 0x10000;
+		new = ft_change(new) + ch;
+	}
+	return (new);
+}
+
+char	*ft_go_space(char *line)
+{
+	while (*line && (*line == ' ' || *line == '\t'))
+		line++;
+	return (line);
+}
+
+void	ft_name_comment(t_my inf, char *define, char **buf)
+{
+	    /// CHECK Define
+	t_text	*ukaz;
+
+	ukaz = inf.head;
+	if (!ukaz || !ukaz->next)
+	{
+		ft_printf("Error. Too small file. Not enough information\n");
+		exit(1);
+	}
+	ukaz = (*(define + 1) == 'n') ? ukaz : ukaz->next;
+    if (!(ukaz->line == ft_strstr(ukaz->line, define)))
+    {
+        ft_printf("Error. Command line should starts with \"%s\"\n", define);
+        exit(1);
+    }
+    ukaz->line += ft_strlen(define);
+	ukaz->line = ft_go_space(ukaz->line);
+	if (*(ukaz->line) != '"')
+	{
+		ft_printf("Error. %s should starts with \"\n", define + 1);
+		exit(1);
+	}
+    ukaz->line++;
+
+	/// WRITE Define
+    int i = 0;
+    while (ukaz->line[i] && ukaz->line[i] != '"')
+    {
+		(*buf)[i] = ukaz->line[i];
+		if (ukaz->line[i] =='\\')
+		{
+			i++;
+			(*buf)[i] = ukaz->line[i];
+		}
+        i++;
+    }
+	if (ukaz->line[i] != '"')
+	{
+		ft_printf("Error. %s should ends with \"\n", define + 1);
+		exit(1);
+	}
+	ukaz->line += i + 1;
+	ukaz->line = ft_go_space(ukaz->line);
+	if (*(ukaz->line) != '\0')
+	{
+		ft_printf("Error. Excess information after %s\n", define + 1);
+		exit(1);
+	}
+}
+
+void		ft_read_file(t_my inf, char *name)
+{
+//	char comment[2052] = {0};
+//	char name2[132] = {0};
+	char *name2;
+	char *comment;
+
+	name2 = ft_memalloc(132);
+	comment = ft_memalloc(2052);
+	/// MAGIC NUMBER
+	int su;
+
+	su = convert_end(COREWAR_EXEC_MAGIC);
+	if ((g_fd = open(name, O_WRONLY | O_CREAT | O_TRUNC,
+					   S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH)) == -1)
+	{
+		ft_printf("error: open\n");
+		exit(1);
+	}
+	write(g_fd, &su, sizeof(su));
+
+    /// CHECK NAME
+	ft_name_comment(inf, NAME_CMD_STRING, &name2);
+	ft_printf("NAME %s\n", name2);
+	/// CHECK COMMENT
+	ft_name_comment(inf, COMMENT_CMD_STRING, &comment);
+	ft_printf("COMMENT %s\n", comment);
+//	t_text	*ukaz;
+//
+//    ukaz = inf.head;
+//    if (!(ukaz->line == ft_strstr(ukaz->line, NAME_CMD_STRING)))
+//    {
+//        ft_printf("Error. First line should starts with \".name\"\n");
+//        exit(1);
+//    }
+//    ukaz->line += 5;
+//	ukaz->line = ft_go_space(ukaz->line);
+//	if (*(ukaz->line) != '"')
+//	{
+//		ft_printf("Error. Name should starts with \"\n");
+//		exit(1);
+//	}
+//    ukaz->line++;
+//
+//	/// WRITE NAME
+//    int i = 0;
+//    char newline[132] = {0};
+//    while (ukaz->line[i] && ukaz->line[i] != '"')
+//    {
+//        newline[i] = ukaz->line[i];
+//		if (ukaz->line[i] =='\\')
+//		{
+//			i++;
+//			newline[i] = ukaz->line[i];
+//		}
+//        i++;
+//    }
+//	if (ukaz->line[i] != '"')
+//	{
+//		ft_printf("Error. Name should ends with \"\n");
+//		exit(1);
+//	}
+//	ukaz->line += i + 1;
+//	ukaz->line = ft_go_space(ukaz->line);
+//	if (*(ukaz->line) != '\0')
+//	{
+//		ft_printf("Error. Excess information after name\n");
+//		exit(1);
+//	}
+
+    /// BOTSIZE
+
+    char *botsize = "a"; // botsize nujno budet naiti!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    write(g_fd, &name2,sizeof(name2));
+    write(g_fd, &(botsize), sizeof(2)); // botsize nujno budet naiti!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+///// CHECK COMMENT
+//
+//	if (!ukaz->next)
+//	{
+//		ft_printf("Error. Too small file. Not enough information\n");
+//		exit(1);
+//	}
+//	ukaz = ukaz->next;
+//	if (!(ukaz->line == ft_strstr(ukaz->line, COMMENT_CMD_STRING)))
+//	{
+//		ft_printf("Error. Comment line should starts with \".comment\"\n");
+//		exit(1);
+//	}
+//	ukaz->line += 8;
+//	ukaz->line = ft_go_space(ukaz->line);
+//	if (*(ukaz->line) != '"')
+//	{
+//		ft_printf("Error. Comment should starts with \"\n");
+//		exit(1);
+//	}
+//	ukaz->line++;
+//
+//
+//	/// WRITE COMMENT
+//	char comment[2052] = {0};
+//	i = 0;
+//	while (ukaz->line[i] && ukaz->line[i] != '"')
+//	{
+//		newline[i] = ukaz->line[i];
+//		if (ukaz->line[i] =='\\')
+//		{
+//			i++;
+//			newline[i] = ukaz->line[i];
+//		}
+//		i++;
+//	}
+//	if (ukaz->line[i] != '"')
+//	{
+//		ft_printf("Error. Name should ends with \"\n");
+//		exit(1);
+//	}
+//	ukaz->line += i + 1;
+//	ukaz->line = ft_go_space(ukaz->line);
+//	if (*(ukaz->line) != '\0')
+//	{
+//		ft_printf("Error. Excess information after name\n");
+//		exit(1);
+//	}
+
+    write(g_fd, &comment,sizeof(comment));
+
+}
+
+void	ft_obnul(t_my	*inf, char *name)
+{
+    inf->fd = open(name, O_RDONLY);
+    inf->head = (t_text*)malloc(sizeof(t_text));
+    inf->head->line = NULL;
+    inf->head->next = NULL;
+
+}
+
+void    ft_push_back(t_my *my, t_text *new, int i)
+{
+    t_text  *p_t;
+
+    p_t = my->head;
+    new->i = i;
+    while (p_t->next)
+    {
+        p_t = p_t->next;
+    }
+    if (p_t->line)
+        p_t->next = new;
+    else
+        my->head = new;
+}
+
+void    ft_print_txt(t_text *t)
+{
+    t_text      *p_t;
+
+    p_t = t;
+    while (p_t->next)
+    {
+        ft_printf("%s\t\n", p_t->line);
+        p_t = p_t->next;
+    }
+    if(p_t)
+        ft_printf("%s\t\n", p_t->line);
+}
+
+void    ft_add_index(t_my   *inf)
+{
+    t_text  *p;
+    int     i;
+
+    i = 1;
+    p = inf->head;
+    while (p)
+    {
+        p->i = i;
+        p = p->next;
+        i++;
+    }
+}
+
+
+int		main(int ac, char **av)
+{
+	char *name;
+    t_my	inf;
+    t_text  *new_t;
+    int     i;
+
+    i = 1;
+    if (ac < 2 || ac > 3)
+		return (ft_printf("Invalid number of arguements")); // inform invalid number of arguement
+	if (!ft_check_format(av[1]))
+		return(ft_printf("Can'head read source file\n"));
+	else
+		name = ft_get_name(av[1]);
+
+
+    //reading
+    ft_obnul(&inf, av[1]);
+    new_t = (t_text*)malloc(sizeof(t_text));
+    new_t->next = NULL;
+    while (get_next_line(inf.fd, &(new_t->line)) > 0)
+    {
+        ft_push_back(&inf, new_t, i++);
+        new_t = (t_text*)malloc(sizeof(t_text));
+        new_t->next = NULL;
+    }
+    free(new_t);
+    ft_print_txt(inf.head);
+
+	ft_read_file(inf, name);
+	ft_printf("Writing output program to %s", name);
+	free(name);
+	return (0);
+}
