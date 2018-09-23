29e67b14b123798c0c884d1b1eef3618181fe2ca
Pavlo OPANASENKO <popanase@e1r7p7.unit.ua>
2018-09-23 15:40:33 +0300

    norme .h

--- t_op.h
+++ t_op.h
@@ -1,25 +1,30 @@
-//
-// Created by Vitalii LEVKO on 8/12/18.
-//
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   t_op.h                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: popanase <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/09/23 15:35:04 by popanase          #+#    #+#             */
+/*   Updated: 2018/09/23 15:36:48 by popanase         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #ifndef COREWAR_T_OP_H
-#define COREWAR_T_OP_H
+# define COREWAR_T_OP_H
+# include "op.h"
 
-#include "op.h"
-
-
-typedef struct s_op
+typedef struct	s_op
 {
-	char *op;
-	short qt_arg;
-	unsigned char arg[3];
-	short code;
-	short cycle;
-	char *exp;
-	short codage;
-	char lable;
-} t_op;
+	char			*op;
+	short			qt_arg;
+	unsigned char	arg[3];
+	short			code;
+	short			cycle;
+	char			*exp;
+	short			codage;
+	char			lable;
+}				t_op;
 
-extern t_op g_op_tab[17];
-
-#endif //COREWAR_T_OP_H
+extern			t_op g_op_tab[17];
+#endif
