1f2d25fac1f0db8993723fe3e7cebd7226b60774
Pavlo OPANASENKO <popanase@e1r7p10.unit.ua>
2018-09-24 14:50:03 +0300

    del all not good

--- test.sh
+++
@@ -1,9 +0,0 @@
-i=0
-while [ $i -le 300000 ]
-do
-   echo $i;
-  i=$(( i + 101));
-  ./corewar -dump $i $1 $2 $3 $4 > test1
-  ./vm_champs/corewar -d $i $1 $2 $3 $4 > test2
-  diff test1 test2;
-done
--- testK.sh
+++
@@ -1,9 +0,0 @@
-i=1600
-while [ $i -le 20000 ]
-do
-   echo $i;
-  i=$(( i + 1));
-  ./corewar -dump $i t_champs/Douceur-power t_champs/Douceur-power t_champs/Douceur-power > test1
-  ./vm_champs/corewar -d $i t_champs/Douceur-power t_champs/Douceur-power t_champs/Douceur-power > test2
-  diff test1 test2;
-done
